1db188ec882adf31a796dbc849de00353d58e353
vmartinvega <vicente.martin.vega@gmail.com>
2020-03-11 21:40:33 +0100

    Added heketi-cli install

---
+++ install-heketi-cli.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Copyright (c) 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+HEKETI_BIN="heketi-cli"      # heketi or heketi-cli
+HEKETI_VERSION="9.0.0"       # latest heketi version => https://github.com/heketi/heketi/releases
+HEKETI_OS="linux"            # linux or darwin
+
+curl -SL https://github.com/heketi/heketi/releases/download/v${HEKETI_VERSION}/heketi-v${HEKETI_VERSION}.${HEKETI_OS}.amd64.tar.gz -o /tmp/heketi-v${HEKETI_VERSION}.${HEKETI_OS}.amd64.tar.gz && \
+tar xzvf /tmp/heketi-v${HEKETI_VERSION}.${HEKETI_OS}.amd64.tar.gz -C /tmp && \
+rm -vf /tmp/heketi-v${HEKETI_VERSION}.${HEKETI_OS}.amd64.tar.gz && \
+sudo cp /tmp/heketi/${HEKETI_BIN} /usr/local/bin/${HEKETI_BIN}_${HEKETI_VERSION} && \
+rm -vrf /tmp/heketi && \
+cd /usr/local/bin && \
+sudo ln -vsnf ${HEKETI_BIN}_${HEKETI_VERSION} ${HEKETI_BIN} && cd
+
+unset HEKETI_BIN HEKETI_VERSION HEKETI_OS
